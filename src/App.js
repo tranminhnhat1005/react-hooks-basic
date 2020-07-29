38afcc6e9e3d8cea6581a56fca90aca5ca2e064f
tran nhat <tranminhnhat1005@gmail.com>
2020-07-29 09:43:33 +0700

    Add component TodoForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App () {
   const [todoList, setTodoList] = useState ([
@@ -20,9 +21,23 @@ function App () {
     setTodoList (newTodoList);
   }
 
+  function handleTodoFormSubmit (formValues) {
+    console.log ('Form submit: ', formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+
+    const newTodoList = [...todoList];
+    newTodoList.push (newTodo);
+    setTodoList (newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>Bayern Munich target this season</h1>
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
